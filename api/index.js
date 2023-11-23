3a0c6cfea46fdfae50a1c8eb46becae2e79dcf35
RyoCa <ryo.ca.dev@gmail.com>
2023-11-23 13:33:22 +0900

    cors

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const postRouter = require('./posts')
 
 const app = express()
 
 // middleware
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
