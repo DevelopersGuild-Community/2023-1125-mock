93cf000912ea122de9d392fd4802c4368993d157
RyoCa <ryo.ca.dev@gmail.com>
2023-11-23 20:53:44 +0900

    add types

---
+++ types/index.ts
@@ -0,0 +1,13 @@
+export type PostItem = {
+  id: string
+  userName: string
+  userImageUrl: string
+  text: string
+  imageUrl: string
+}
+
+export type Profile = {
+  id: string
+  userName: string
+  imageUrl: string
+}
